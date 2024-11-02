c70d0704f6ba837727abb8d0ca01fd40584beea4
cbrown <vanguard737@gmail.com>
2024-11-02 05:13:38 -0500

    black/isort/ruff

--- setup.py
+++ setup.py
@@ -1,15 +1,28 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 setup(
-    name='colabdesign',
-    version='1.1.2',
-    description='Making Protein Design accessible to all via Google Colab!',
+    name="colabdesign",
+    version="1.1.2",
+    description="Making Protein Design accessible to all via Google Colab!",
     long_description="Making Protein Design accessible to all via Google Colab!",
-    long_description_content_type='text/markdown',
-    packages=find_packages(include=['colabdesign*']),
-    install_requires=['py3Dmol','absl-py','biopython',
-                      'chex','dm-haiku','dm-tree',
-                      'immutabledict','jax','ml-collections',
-                      'numpy','pandas','scipy','optax','joblib',
-                      'matplotlib'],
-    include_package_data=True
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["colabdesign*"]),
+    install_requires=[
+        "py3Dmol",
+        "absl-py",
+        "biopython",
+        "chex",
+        "dm-haiku",
+        "dm-tree",
+        "immutabledict",
+        "jax",
+        "ml-collections",
+        "numpy",
+        "pandas",
+        "scipy",
+        "optax",
+        "joblib",
+        "matplotlib",
+    ],
+    include_package_data=True,
 )
